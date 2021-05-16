cb25bfa676eeabc1e8825f99060ea77402abf519
Paolo <jplojo34@gmail.com>
2021-05-17 03:52:04 +0800

    get admin

--- routes/routes.js
+++ routes/routes.js
@@ -2,11 +2,13 @@ const express = require(`express`);
 
 const controller = require(`../controllers/controller.js`);
 const SignUpcontroller = require(`../controllers/SignUpcontroller.js`);
+const Admincontroller = require(`../controllers/Admincontroller.js`);
 
 const app = express();
 
 app.get(`/`, controller.getIndex);
 app.get(`/login`, SignUpcontroller.getLogin);
 app.get(`/register`, SignUpcontroller.getRegister);
+app.get('/admin', Admincontroller.getAdmin)
 
 module.exports = app;
