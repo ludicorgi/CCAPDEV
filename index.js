f2bd227a728f118813f12af9ee00de1e24ab5daf
Paolo <jplojo34@gmail.com>
2021-05-22 13:08:15 +0800

    express-sessions included in index.js

--- index.js
+++ index.js
@@ -1,26 +1,53 @@
 const dotenv = require('dotenv');
-const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require(`./routes/routes.js`);
 const db = require(`./models/db.js`);
 const hbs = require(`hbs`);
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const mongoStore = require('connect-mongodb-session')(session);
 
+
+dotenv.config();
+port = process.env.PORT;
+hostname = process.env.HOSTNAME;
+dbUri = process.env.DB_URL;
 db.connect();
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.urlencoded({extended: true}))
+app.use(cookieParser());
 
-dotenv.config();
-port = process.env.PORT;
-hostname = process.env.HOSTNAME;
+const store = new mongoStore({
+    uri: dbUri,
+    databaseName: 'ShoppingApp',
+    collection: 'session'
+});
 
 app.set(`view engine`, `hbs`);
 hbs.registerPartials(__dirname + `/views/partials`);
 
 app.use(express.static('public'));
 app.use(`/`, routes);
+
+app.use(session({
+    key: 'user_sid',
+    secret: 'secret-key',
+    resave: false,
+    saveUninitialized: false,
+    rolling: true,     
+    cookie: {
+        maxAge: 12096e5
+    },
+    store: store
+}));
+
+app.use((req, res, next) => {
+    res.locals.session = req.session;
+    next();
+});
 
 app.listen(port, hostname, function(){
     console.log(`Server running at:`);
