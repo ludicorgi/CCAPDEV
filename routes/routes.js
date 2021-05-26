3de77bd62a7aedd47b1f8e00d329f49966466a14
j3jabanes <70612391+j3jabanes@users.noreply.github.com>
2021-05-26 21:44:35 +0800

    npm start async update
    asnychronously updates edits made to js /dbfiles

--- routes/routes.js
+++ routes/routes.js
@@ -3,14 +3,21 @@ const express = require(`express`);
 const controller = require(`../controllers/controller.js`);
 const SignUpcontroller = require(`../controllers/SignUpcontroller.js`);
 const Admincontroller = require(`../controllers/Admincontroller.js`);
-
+const Productcontroller = require(`../controllers/Productcontroller.js`);
 const app = express();
 
+// index
 app.get(`/`, controller.getIndex);
+
+// Signup
 app.get(`/login`, SignUpcontroller.getLogin);
 app.get(`/register`, SignUpcontroller.getRegister);
 app.get('/edit_profile', SignUpcontroller.getMyAccount);
-app.get('/admin', Admincontroller.getAdmin)
 
+// Admin
+app.get('/admin', Admincontroller.getAdmin);
+
+// Products
+app.get('/products', Productcontroller.getProduct);
 
 module.exports = app;
