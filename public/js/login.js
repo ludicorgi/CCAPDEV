38f699869f78e46d43332520fb5b311b129111f6
Paolo <jplojo34@gmail.com>
2021-05-27 01:39:10 +0800

    admin accessible from login

--- public/js/login.js
+++ public/js/login.js
@@ -8,11 +8,17 @@ $(document).ready(function () {
             $('#error').text('Fill up all fields');
         }
         else{
-            $.get('/logAcc', {email:email, password:password}, function(result){
+            //TODO: make admin credentials more secure
+            if(email == "admin" && password == "admin"){
+                window.location.assign('/admin');
+            }
+            else{
+                 $.get('/logAcc', {email:email, password:password}, function(result){
                 if(result.email != undefined)
                     window.location.assign('/edit_profile');
                 else $('#error').text("Invalid credentials");
-            });
+                });
+            }
         }
     });
 });
