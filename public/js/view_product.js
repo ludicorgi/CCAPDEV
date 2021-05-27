10a14c6f10d2dcb3f1845b262e8dc4901dceac43
Paolo <jplojo34@gmail.com>
2021-05-27 12:28:13 +0800

    updated view_product buttons

--- public/js/view_product.js
+++ public/js/view_product.js
@@ -1,2 +1,22 @@
 $(document).ready(function () {
+
+    $('#buy').click(function(){
+        $.get('/session', {}, function(result){
+            
+        });
+    });
+
+    $('#send').click(function(){
+        if($('#rating').val() == "" || $('#comment').val() == "")
+            $('#error').text("please fill up all fields");
+        else if($('#rating').val() > 5 || $('#rating').val() < 0)
+            $('#error').text("review score must be between 0 to 5");
+        else {
+            $.get('/session', {}, function(result){
+                /*TODO: 
+                append to comments
+                add review to user's and product's db*/
+            });
+        }
+    });
 });
