ad7ba6b09d470088a4adc7dc8393b5034d938e87
j3jabanes <70612391+j3jabanes@users.noreply.github.com>
2021-06-02 19:21:46 +0800

    Create Searchcontroller.js
    controller for category and search

---
+++ controllers/Searchcontroller.js
@@ -0,0 +1,22 @@
+const db = require('../models/db.js');
+const Products = require('../models/Productmodel.js');
+
+
+const Searchcontroller = {
+    getCategory: function(req, res) {
+        Products.find(req.query, function(err, result){
+            res.render('search_prod', {products: result, title : req.query.tags});
+            
+        });},
+
+    getSearch: function(req, res) {
+        const key = req.query.tags;
+        if(key){
+            const pattern = new RegExp(key, 'i');
+            Products.find({tags:{$regex : pattern}}, function(err, results){
+                res.render('search_prod', {products: results, title : key});
+            });
+        }
+    },
+}
+module.exports = Searchcontroller;
