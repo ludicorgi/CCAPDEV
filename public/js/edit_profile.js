238b36383a467c0f8fb92bfd4663668a5d01f3e2
Paolo <jplojo34@gmail.com>
2021-06-03 15:49:54 +0800

    ccNo and sNo required to be numerical

--- public/js/edit_profile.js
+++ public/js/edit_profile.js
@@ -7,44 +7,49 @@ $(document).ready(function () {
     });
 
     $('#save').click(function(){
-        $('.inputbox').prop('disabled', true);
-        $('#save').prop('disabled', true);
+      var fn,ln,gender,ccNo,ExpDate,address,imgURL;
 
-        var fn,ln,gender,ccNo,ExpDate,address,imgURL;
-
-        if ($('#fn').val() == "")
-           fn = $('#fn').attr('placeholder');
-        else fn = $('#fn').val();
-        
-        if ($('#ln').val() == "")
+      if ($('#fn').val() == "")
+         fn = $('#fn').attr('placeholder');
+      else fn = $('#fn').val();
+      
+      if ($('#ln').val() == "")
             ln = $('#ln').attr('placeholder');
-        else ln = $('#ln').val();
+      else ln = $('#ln').val();
 
-        if ($('#gender').val() == "")
-           gender = $('#gender').attr('placeholder');
-        else gender = $('#gender').val();
+      if ($('#gender').val() == "")
+         gender = $('#gender').attr('placeholder');
+      else gender = $('#gender').val();
 
-        if ($('#ccNo').val() == "")
-           ccNo = $('#ccNo').attr('placeholder');
-        else ccNo = $('#ccNo').val();
+      if ($('#ccNo').val() == "")
+         ccNo = $('#ccNo').attr('placeholder');
+      else ccNo = $('#ccNo').val();
 
-        if ($('#sNo').val() == "")
-           sNo = $('#sNo').attr('placeholder');
-        else sNo = $('#sNo').val();
+      if ($('#sNo').val() == "")
+         sNo = $('#sNo').attr('placeholder');
+      else sNo = $('#sNo').val();
 
-        if ($('#ExpDate').val() == "")
-           ExpDate = $('#ExpDate').attr('placeholder');
-        else ExpDate = $('#ExpDate').val();
+      if ($('#ExpDate').val() == "")
+         ExpDate = $('#ExpDate').attr('placeholder');
+      else ExpDate = $('#ExpDate').val();
 
-        if ($('#address').val() == "")
-           address = $('#address').attr('placeholder');
-        else address = $('#address').val();
+      if ($('#address').val() == "")
+         address = $('#address').attr('placeholder');
+      else address = $('#address').val();
 
-        if ($('#imgURL').val() == "")
+      if ($('#imgURL').val() == "")
             imgURL = $('#imgURL').attr('placeholder');
-        else imgURL = $('#imgURL').val();
+      else imgURL = $('#imgURL').val();
 
-        $.get('/editdetails',{fn:fn, ln:ln, gender:gender, ccNo: ccNo, sNo: sNo, ExpDate:ExpDate, address:address, imgURL: imgURL}, function(){});
-        $('body').load('/edit_profile');
+      if(!($.isNumeric(ccNo)) ||  !($.isNumeric(sNo))){
+         $('#error').text('Credit Card and Security Number must be valid numerals');
+      }
+      else{
+         $('.inputbox').prop('disabled', true);
+         $('#save').prop('disabled', true);
+
+         $.get('/editdetails',{fn:fn, ln:ln, gender:gender, ccNo: ccNo, sNo: sNo, ExpDate:ExpDate, address:address, imgURL: imgURL}, function(){});
+         $('body').load('/edit_profile');
+      }
     });
 });
